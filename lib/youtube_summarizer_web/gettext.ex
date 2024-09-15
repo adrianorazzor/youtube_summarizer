a5793d68b55f016c9c8dffa2cbdb9dd6ba11923d
Adriano Pereira <adrianorazzor@gmail.com>
2024-09-15 17:37:36 -0400

    Adjustes gettex

--- lib/youtube_summarizer_web/gettext.ex
+++ lib/youtube_summarizer_web/gettext.ex
@@ -20,5 +20,5 @@ defmodule YoutubeSummarizerWeb.Gettext do
 
   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
   """
-  use Gettext, otp_app: :youtube_summarizer
+  use Gettext.Backend, otp_app: :youtube_summarizer
 end
